fc4966d107e145797bf817455f828034f8e04860
Skýpala Martin <88775807@cuni.cz>
2026-08-22 12:25:14 +0200

    Finished buttons and stuff, off to the display

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Sorting_algorithms;
 
 public partial class Form1 : Form
@@ -7,6 +10,8 @@ public partial class Form1 : Form
         new AlgorithmInfo { Name = "Bogo Sort", SortMethod = SortingFunctions.BogoSort, Note = "" },
         // Add more algorithms here
     };
+
+
     public Form1()
     {
         InitializeComponent();
@@ -67,7 +72,16 @@ public partial class Form1 : Form
 
         TextBox inputTextBox = new TextBox();
 
-        inputTextBox.Location = new Point(40, 40);
+        Label PleadsForNumber = new Label();
+
+        PleadsForNumber.Name = "PleaseTXT";
+        PleadsForNumber.Text = "Please enter number of elements in array:";
+        PleadsForNumber.Location = new Point(40, 40);
+        PleadsForNumber.AutoSize = true;
+
+        this.Controls.Add(PleadsForNumber);
+
+        inputTextBox.Location = new Point(40, 80);
         inputTextBox.Size = new Size(200, 30);
 
         inputTextBox.Name = "txtInput";
@@ -76,7 +90,7 @@ public partial class Form1 : Form
         this.Controls.Add(inputTextBox);
 
         Button submitButton = new Button();
-        submitButton.Location = new Point(40, 80);
+        submitButton.Location = new Point(40, 120);
         submitButton.Size = new Size(90, 30);
         submitButton.Text = "Submit";
         submitButton.Tag = clickedButton.Tag;
@@ -88,74 +102,109 @@ public partial class Form1 : Form
     private void SubmitButton_Click(object sender, EventArgs e)
     {
         Button clickedButton = sender as Button;
-        
+        int size = 1;
+        Label ToDelete = this.Controls["PleaseTXT"] as Label;
+        this.Controls.Remove(ToDelete);
+        ToDelete.Dispose();
+
         try
         {
-            int size = int.Parse(this.Controls["txtInput"].Text);
-            object sortMethod = clickedButton.Tag;
+            size = int.Parse(this.Controls["txtInput"].Text);
         }
         catch (Exception ex)
         {
             TextBox TB = this.Controls["txtInput"] as TextBox;
-            
+
             this.Controls.Remove(TB);
             TB.Dispose();
 
             this.Controls.Remove(clickedButton);
             clickedButton.Dispose();
-            
+
+            MessageBox.Show("You did not enter the number in the correct format");
+
             LoadInitializeButton();
             return;
         }
-        
+
+        Func<int[], IEnumerable<SortStep>> sortMethod = clickedButton.Tag as Func<int[], IEnumerable<SortStep>>;
+
         TextBox textBox = this.Controls["txtInput"] as TextBox;
+
         this.Controls.Remove(textBox);
         textBox.Dispose();
 
         this.Controls.Remove(clickedButton);
         clickedButton.Dispose();
+
+        //change of plans, the sortin function will return ienumerable steps, that i will read here and display
+
+        //shuffle and display the array
+
+        int[] ToSort = new int[size];
+
+        var rng = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            int j = rng.Next(i + 1);
+            int temp = ToSort[i];
+            ToSort[i] = ToSort[j];
+            ToSort[j] = temp;
+        }
+
+        foreach (var step in sortMethod(ToSort))
+        {
+            
+        }
     }
 }
 public class AlgorithmInfo
 {
     public string Name { get; set; }
-    public Action<int[]> SortMethod { get; set; }
+    public Func<int[], IEnumerable<SortStep>> SortMethod { get; set; }
     public string Note { get; set; } = "";
 }
+public enum SortType { Compare, Swap, Done}
+public class SortStep
+{
+    public int[] Array { get; set; }
+    public SortType SortType { get; set; }
+    public int? IndexA { get; set; }
+    public int? IndexB { get; set; }
+}
+
 class SortingFunctions
 {
 
-    public static void BogoSort(int[] array)
+    public static IEnumerable<SortStep> BogoSort(int[] array)
     {
         Random random = new Random();
-        while (!IsSorted(array))
+        bool IsSorted = false;
+        while (!IsSorted)
         {
-            Shuffle(array, random);
-        }
-    }
-    private static bool IsSorted(int[] array)
-    {
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            //TODO
-            //here i will colour the two elements being compared
-            if (array[i] > array[i + 1])
+            for (int i = array.Length - 1; i > 0; i--)
             {
-                return false;
+                int j = random.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = i, IndexB = j };
+            }
+            IsSorted = true;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = i + 1 };
+                if (array[i] > array[i + 1])
+                {
+                    IsSorted = false;
+                    break;
+                }
             }
         }
-        return true;
+        yield return new SortStep { Array = array, SortType = SortType.Done };
     }
-    private static void Shuffle(int[] array, Random random)
-    {
-        for (int i = array.Length - 1; i > 0; i--)
-        {
-            int j = random.Next(i + 1);
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            //TODO
-            //here i will flip the two elements
-        }
-    }
+
+
 }
