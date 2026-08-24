d10dc3d747ad04abc31585d64f5835fcd57664b4
Skýpala Martin <88775807@cuni.cz>
2026-08-24 12:47:48 +0200

    Some sorting algorithms done, need more

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
@@ -10,6 +11,10 @@ public partial class Form1 : Form
     {
         new AlgorithmInfo { Name = "Bogo Sort", SortMethod = SortingFunctions.BogoSort, Note = "" },
         new AlgorithmInfo {Name = "Bubble Sort", SortMethod = SortingFunctions.BubbleSort, Note = ""},
+        new AlgorithmInfo {Name = "Shaker Sort", SortMethod = SortingFunctions.ShakerSort, Note = ""},
+        new AlgorithmInfo {Name = "Selection Sort", SortMethod = SortingFunctions.SelectSort, Note = ""},
+        new AlgorithmInfo {Name = "Double Selection Sort", SortMethod = SortingFunctions.MinMaxSelect, Note = ""},
+        new AlgorithmInfo {Name = "Heap Sort", SortMethod = SortingFunctions.HeapSort, Note = ""}
         // Add more algorithms here
     };
 
@@ -46,8 +51,24 @@ public partial class Form1 : Form
             DynamicButton.Text = algorithm.Name;
             DynamicButton.Name = "btn" + algorithm.Name.Replace(" ", "");
 
-            DynamicButton.Location = new Point(40, 40 + algorithms.IndexOf(algorithm) * 40);
-            DynamicButton.Size = new Size(90, 30);
+            int X = 40;
+            int Y = 40 + algorithms.IndexOf(algorithm) * 40;
+
+            int NormalizeWith = (this.ClientSize.Height - 30) - (this.ClientSize.Height - 30) % 40;
+
+            if (NormalizeWith == 0)
+            {
+                NormalizeWith = 40;
+            }
+
+            while (Y > this.ClientSize.Height - 30)
+            {
+                X += 160;
+                Y -= NormalizeWith;
+            }
+
+            DynamicButton.Location = new Point(X, Y);
+            DynamicButton.Size = new Size(150, 30);
 
             DynamicButton.Click += new EventHandler(SortButton_Click);
             DynamicButton.Tag = algorithm.SortMethod;
@@ -172,8 +193,30 @@ public partial class Form1 : Form
         System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
 
         timer.Tick += Timer_Tick;
+        timer.Disposed += Timer_Disposed;
         timer.Interval = 1;
         timer.Start();
+    }
+
+    private void Timer_Disposed(object? sender, EventArgs e)
+    {
+        Button End = new Button();
+        End.Size = new Size(120, 30);
+        End.Text = "Back to teh start";
+        End.Name = "btnEnd";
+        End.Location = new Point(Width / 2 - 60, Height / 2 - 15);
+
+        End.Click += End_Click; ;
+
+        this.Controls.Add(End);
+        End.BringToFront();
+    }
+
+    private void End_Click(object? sender, EventArgs e)
+    {
+        this.Controls.Clear();
+
+        LoadInitializeButton();
     }
 
     private void Timer_Tick(object? sender, EventArgs e)
@@ -187,6 +230,7 @@ public partial class Form1 : Form
         else
         {
             timer.Stop();
+            timer.Dispose();
         }
     }
 }
@@ -231,20 +275,20 @@ public class VisualizationPanel : Panel
                     int j = (int)CurrentStep.IndexB;
                     var rect = new Rectangle();
                     rect.Size = new Size(
-                        (i + 2) * Width / CurrentStep.Array.Length - (i + 1) * Width / CurrentStep.Array.Length,
+                        (i + 1) * Width / CurrentStep.Array.Length - i * Width / CurrentStep.Array.Length,
                         Height * CurrentStep.Array[i] / CurrentStep.Array.Length);
 
                     rect.Location = new Point(
-                        (i + 1) * Width / CurrentStep.Array.Length,
+                        i * Width / CurrentStep.Array.Length,
                         Height - Height * CurrentStep.Array[i] / CurrentStep.Array.Length);
 
                     var rect2 = new Rectangle();
                     rect2.Size = new Size(
-                        (j + 2) * Width / CurrentStep.Array.Length - (j + 1) * Width / CurrentStep.Array.Length,
+                        (j + 1) * Width / CurrentStep.Array.Length - j * Width / CurrentStep.Array.Length,
                         Height * CurrentStep.Array[j] / CurrentStep.Array.Length);
 
                     rect2.Location = new Point(
-                        (j + 1) * Width / CurrentStep.Array.Length,
+                        j * Width / CurrentStep.Array.Length,
                         Height - Height * CurrentStep.Array[j] / CurrentStep.Array.Length);
 
                     {
@@ -260,20 +304,20 @@ public class VisualizationPanel : Panel
                     int j = (int)CurrentStep.IndexB;
                     var rect = new Rectangle();
                     rect.Size = new Size(
-                        (i + 2) * Width / CurrentStep.Array.Length - (i + 1) * Width / CurrentStep.Array.Length,
+                        (i + 1) * Width / CurrentStep.Array.Length - i * Width / CurrentStep.Array.Length,
                         Height * CurrentStep.Array[i] / CurrentStep.Array.Length);
 
                     rect.Location = new Point(
-                        (i + 1) * Width / CurrentStep.Array.Length,
+                        i * Width / CurrentStep.Array.Length,
                         Height - Height * CurrentStep.Array[i] / CurrentStep.Array.Length);
 
                     var rect2 = new Rectangle();
                     rect2.Size = new Size(
-                        (j + 2) * Width / CurrentStep.Array.Length - (j + 1) * Width / CurrentStep.Array.Length,
+                        (j + 1) * Width / CurrentStep.Array.Length - j * Width / CurrentStep.Array.Length,
                         Height * CurrentStep.Array[j] / CurrentStep.Array.Length);
 
                     rect2.Location = new Point(
-                        (j + 1) * Width / CurrentStep.Array.Length,
+                        j * Width / CurrentStep.Array.Length,
                         Height - Height * CurrentStep.Array[j] / CurrentStep.Array.Length);
 
                     {
@@ -331,11 +375,12 @@ class SortingFunctions
             for (int i = 0; i < array.Length; i++)
             {
                 int j = random.Next(0, array.Length);
+
+                yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = j };
+
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
-
-                yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = j };
             }
             IsSorted = true;
             for (int i = 0; i < array.Length - 1; i++)
@@ -363,16 +408,167 @@ class SortingFunctions
                 yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = j, IndexB = j + 1 };
                 if (array[j] > array[j + 1])
                 {
+                    yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = j, IndexB = j + 1 };
+
                     IsSorted = false;
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
-
-                    yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = j, IndexB = j + 1 };
                 }
             }
             i++;
         }
         yield return new SortStep { Array = array, SortType = SortType.Done };
     }
+    public static IEnumerable<SortStep> ShakerSort(int[] array)
+    {
+        bool IsSorted = false;
+        int i = 1;
+        while (!IsSorted)
+        {
+            IsSorted = true;
+            for (int j = i - 1; j < array.Length - i; j++)
+            {
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = j, IndexB = j + 1 };
+                if (array[j] > array[j + 1])
+                {
+                    yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = j, IndexB = j + 1 };
+
+                    IsSorted = false;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = array.Length - 1 - j, IndexB = array.Length - j - 2 };
+                if (array[array.Length - j - 1] < array[array.Length - j - 2])
+                {
+                    yield return new SortStep { Array = array, IndexA = array.Length - j - 1, IndexB = array.Length - j - 2 };
+
+                    IsSorted = false;
+                    int temp = array[array.Length - j - 1];
+                    array[array.Length - j - 1] = array[array.Length - j - 2];
+                    array[array.Length - j - 2] = temp;
+                }
+            }
+            i++;
+        }
+        yield return new SortStep { Array = array, SortType = SortType.Done };
+    }
+    public static IEnumerable<SortStep> SelectSort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int max = array[0];
+            int maxIndex = 0;
+            for (int j = 0; j < array.Length - i; j++)
+            {
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = j, IndexB = maxIndex };
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    maxIndex = j;
+                }
+            }
+            yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = array.Length - 1 - i, IndexB = maxIndex };
+
+            int temp = array[array.Length - 1 - i];
+            array[array.Length - 1 - i] = max;
+            array[maxIndex] = temp;
+        }
+        yield return new SortStep { Array = array, SortType = SortType.Done };
+    }
+    public static IEnumerable<SortStep> MinMaxSelect(int[] array)
+    {
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            int min = array[i];
+            int minIndex = i;
+            int max = array[i];
+            int maxIndex = i;
+            for (int j = i; j < array.Length - i; j++)
+            {
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = j, IndexB = minIndex };
+                if (array[j] < min)
+                {
+                    min = array[j];
+                    minIndex = j;
+                }
+
+                yield return new SortStep { Array = array, SortType = SortType.Compare, IndexA = j, IndexB = maxIndex };
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    maxIndex = j;
+                }
+            }
+            yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = array.Length - 1 - i, IndexB = maxIndex };
+
+            int temp = array[array.Length - 1 - i];
+            array[array.Length - 1 - i] = array[maxIndex];
+            array[maxIndex] = temp;
+
+            if (minIndex == array.Length - 1 - i)
+            {
+                minIndex = maxIndex;
+            }
+
+            yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = minIndex, IndexB = i };
+
+            temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        yield return new SortStep { Array = array, SortType = SortType.Done };
+    }
+    public static IEnumerable<SortStep> HeapSort(int[] array)
+    {
+        //sestaveni haldy
+
+        for (int z = 0; z < array.Length; z++)
+        {
+            int temp = array[z];
+            int i = z;
+
+            while (i > 0 && temp > array[(i - 1) / 2])
+            {
+                yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = (i - 1) / 2 };
+                array[i] = array[(i - 1) / 2];
+                i = (i - 1) / 2;
+            }
+            array[i] = temp;
+
+        }
+
+        //trideni
+
+        for (int z = array.Length - 1; z > 0; z--)
+        {
+            int temp = array[z];
+            yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = z, IndexB = 0 };
+            array[z] = array[0];
+            int i = 0;
+
+            while ((2 * i + 1 < z && array[2 * i + 1] > temp) ||
+                    (2 * i + 2 < z && array[2 * i + 2] > temp))
+            {
+                if (2 * i + 2 < z && array[2 * i + 1] < array[2 * i + 2])
+                {
+                    yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = 2 * i + 2 };
+                    array[i] = array[2 * i + 2];
+                    i = 2 * i + 2;
+                }
+                else
+                {
+                    yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = 2 * i + 1 };
+                    array[i] = array[2 * i + 1];
+                    i = 2 * i + 1;
+                }
+            }
+            array[i] = temp;
+
+            yield return new SortStep { Array = array, SortType = SortType.Swap, IndexA = i, IndexB = z };
+        }
+        yield return new SortStep { Array = array, SortType = SortType.Done };
+    }
+
 }
